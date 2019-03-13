5712b99b6aade462d22e2d1fa7c5300d2a66a69f
Ubuntu <ubuntu@ip-172-31-42-17.ec2.internal>
2019-03-13 04:01:39 +0000

    Schlierin mp4

--- module1/JiaweiZhuang/png_to_mp4.sh
+++ module1/JiaweiZhuang/png_to_mp4.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ffmpeg -framerate 12 -i ./movie/frame_%03d.png -pix_fmt yuv420p movie_temperature.mp4
+# ffmpeg -framerate 12 -i ./movie/frame_%03d.png -pix_fmt yuv420p movie_temperature.mp4
+
+ffmpeg -framerate 12 -i ./movie_schlieren/frame_sch_%03d.png -pix_fmt yuv420p movie_schlieren.mp4
